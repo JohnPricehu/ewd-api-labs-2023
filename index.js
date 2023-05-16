39dddc9d657b9aa9a920704177e055393a52e6ca
Yiming Hu <20092158@mail.wit.ie>
2023-05-16 18:19:06 +0000

    Added Accounts Endpoints

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './src/movies';
 import genresRouter from './src/genres';
+import createAccountsRouter from './src/accounts/routes';
+import buildDependencies from "./src/config/dependencies";
 
 dotenv.config();
 
-const app = express();
+//.. Add this after db.init();
 
 const port = process.env.PORT;
 
+const dependencies = buildDependencies();
+
+const app = express();
 app.use('/api/movies', moviesRouter);
 app.use('/api/genres', genresRouter);
+app.use('/api/accounts', createAccountsRouter(dependencies));
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
