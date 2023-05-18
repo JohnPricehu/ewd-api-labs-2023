a2a5e847dc8fe963ccdc5640e60a5753a65ebf66
Yiming Hu <20092158@mail.wit.ie>
2023-05-18 13:49:47 +0000

    Implement lab3 exercises

--- src/accounts/controllers/index.js
+++ src/accounts/controllers/index.js
@@ -2,12 +2,25 @@ import accountService from "../services";
 
 export default (dependencies) => {
 
+    const passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\S+$).{7,20}$/;
+    
     const createAccount = async (request, response, next) => {
-        // Input
         const { firstName, lastName, email, password } = request.body;
-        // Treatment
+
+        // Adding validation for firstName and lastName length
+        if (firstName.length < 1 || firstName.length > 30) {
+            return response.status(400).send({ error: 'FirstName must be between 1 and 30 characters.' });
+        }
+        if (lastName.length < 1 || lastName.length > 30) {
+            return response.status(400).send({ error: 'LastName must be between 1 and 30 characters.' });
+        }
+
+        // Validate password strength
+        if (!passwordRegex.test(password)) {
+            return response.status(400).send({ error: 'Password does not meet the strength requirements.' });
+        }
+
         const account = await accountService.registerAccount(firstName, lastName, email, password, dependencies);
-        //output
         response.status(201).json(account)
     };
     const getAccount = async (request, response, next) => {
@@ -49,12 +62,20 @@ export default (dependencies) => {
         try {
             const { movieId } = request.body;
             const id = request.params.id;
-            const account = await accountService.addFavourite(id, movieId, dependencies);
-            response.status(200).json(account);
+            const account = await accountService.getAccount(id, dependencies);
+            
+            // Preventing duplicate favorites
+            if (account.favourites.includes(movieId)) {
+                return response.status(400).send({ error: 'This movie is already a favourite.' });
+            }
+
+            const updatedAccount = await accountService.addFavourite(id, movieId, dependencies);
+            response.status(200).json(updatedAccount);
         } catch (err) {
             next(new Error(`Invalid Data ${err.message}`));
         }
     };
+
     const getFavourites = async (request, response, next) => {
         try {
             const id = request.params.id;
