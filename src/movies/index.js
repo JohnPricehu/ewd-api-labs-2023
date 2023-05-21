99638c1ea3ede0610dc93428464a5d926658739e
Yiming Hu <20092158@mail.wit.ie>
2023-05-21 12:32:51 +0000

    Update Movies index

--- src/movies/index.js
+++ src/movies/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import {movies, movieReviews, movieDetails} from './moviesData';
+import {movies, movieReviews, movieDetails, movieImages, movieCast, similarMovies} from './moviesData';
 
 const router = express.Router(); 
+
 router.get('/', (req, res) => {
     res.json(movies);
 });
@@ -22,7 +23,6 @@ router.get('/:id', (req, res) => {
 // Get movie reviews
 router.get('/:id/reviews', (req, res) => {
     const id = parseInt(req.params.id);
-    // find reviews in list
     if (movieReviews.id == id) {
         res.status(200).json(movieReviews);
     } else {
@@ -33,5 +33,43 @@ router.get('/:id/reviews', (req, res) => {
     }
 }); 
 
+// Get movie images
+router.get('/:id/images', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (movieImages.id == id) {
+        res.status(200).json(movieImages);
+    } else {
+        res.status(404).json({
+            message: 'The resource you requested could not be found.',
+            status_code: 404
+        });
+    }
+});
+
+// Get movie cast
+router.get('/:id/cast', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (movieCast.id == id) {
+        res.status(200).json(movieCast);
+    } else {
+        res.status(404).json({
+            message: 'The resource you requested could not be found.',
+            status_code: 404
+        });
+    }
+});
+
+// Get similar movies
+router.get('/:id/similar', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (similarMovies.id == id) {
+        res.status(200).json(similarMovies);
+    } else {
+        res.status(404).json({
+            message: 'The resource you requested could not be found.',
+            status_code: 404
+        });
+    }
+});
 
 export default router;
