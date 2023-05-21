a1d175bcb393d22274ded364ab24260a39048c07
Yiming Hu <20092158@mail.wit.ie>
2023-05-21 12:50:40 +0000

    Implement actors services

---
+++ src/actors/services/index.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export default {
+    getActor: async(personId) => {
+        const response = await axios.get(`https://api.themoviedb.org/3/person/${personId}?api_key=${process.env.TMDB_KEY}`);
+        return response.data;
+    },
+    find: async (query) => {
+        const response = await axios.get(
+            `https://api.themoviedb.org/3/person/popular?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&${query}`
+        );
+        return response.data;
+    },
+    getActorMovies: async (personId) => {
+        const response = await axios.get(
+            `https://api.themoviedb.org/3/person/${personId}/movie_credits?api_key=${process.env.TMDB_KEY}`
+        );
+        return response.data;
+    },
+    getActorImages: async (personId) => {
+        const response = await axios.get(
+            `https://api.themoviedb.org/3/person/${personId}/images?api_key=${process.env.TMDB_KEY}`
+        );
+        return response.data;
+    },
+};
