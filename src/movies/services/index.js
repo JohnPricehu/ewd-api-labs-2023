ebc41027387a8f4345ece54d55642b9eac97d300
Yiming Hu <20092158@mail.wit.ie>
2023-05-21 12:29:34 +0000

    Update Movies services

--- src/movies/services/index.js
+++ src/movies/services/index.js
@@ -4,19 +4,42 @@ export default {
     getMovie: async (movieId) => {
         const response = await axios.get(
             `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.TMDB_KEY}`
-          );
-          return response.data;
+        );
+        return response.data;
     },
     find: async (query) => {
         const response = await axios.get(
             `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&${query}`
-          );
-          return response.data;
+        );
+        return response.data;
     },
     findUpcoming: async () => {
-      const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false`;
-      const response = await axios.get(url);
-      return response.data;
+        const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false`;
+        const response = await axios.get(url);
+        return response.data;
     },
-
-  };
+    getMovieImages: async (movieId) => {
+        const response = await axios.get(
+            `https://api.themoviedb.org/3/movie/${movieId}/images?api_key=${process.env.TMDB_KEY}`
+        );
+        return response.data;
+    },
+    getMovieCast: async (movieId) => {
+        const response = await axios.get(
+            `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${process.env.TMDB_KEY}`
+        );
+        return response.data;
+    },
+    getMovieReviews: async (movieId) => {
+        const response = await axios.get(
+            `https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${process.env.TMDB_KEY}`
+        );
+        return response.data;
+    },
+    getSimilarMovies: async (movieId) => {
+        const response = await axios.get(
+            `https://api.themoviedb.org/3/movie/${movieId}/similar?api_key=${process.env.TMDB_KEY}`
+        );
+        return response.data;
+    },
+};
