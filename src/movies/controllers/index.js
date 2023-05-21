d2c0928cd315b4025f9cd7553445efc005e83bdf
Yiming Hu <20092158@mail.wit.ie>
2023-05-21 12:18:52 +0000

    Update Movies controllers

--- src/movies/controllers/index.js
+++ src/movies/controllers/index.js
@@ -26,10 +26,37 @@ export default (dependencies) => {
           next(error);
         }
       };
+    const getMovieImages = async (request, response, next) => {
+        const movieId = request.params.id;
+        const images = await moviesService.getMovieImages(movieId, dependencies);
+        response.status(200).json(images);
+    };
+
+    const getMovieCast = async (request, response, next) => {
+        const movieId = request.params.id;
+        const cast = await moviesService.getMovieCast(movieId, dependencies);
+        response.status(200).json(cast);
+    };
+
+    const getMovieReviews = async (request, response, next) => {
+        const movieId = request.params.id;
+        const reviews = await moviesService.getMovieReviews(movieId, dependencies);
+        response.status(200).json(reviews);
+    };
+
+    const getSimilarMovies = async (request, response, next) => {
+        const movieId = request.params.id;
+        const similarMovies = await moviesService.getSimilarMovies(movieId, dependencies);
+        response.status(200).json(similarMovies);
+    };
       
     return {
         getMovie,
         find,
-        getUpcomingMovies
+        getUpcomingMovies,
+        getMovieImages,
+        getMovieCast,
+        getMovieReviews,
+        getSimilarMovies
     };
 };
