972700b41c70b3d1d4c2d3a55d75e79e97052409
Yiming Hu <20092158@mail.wit.ie>
2023-05-22 17:17:29 +0000

    Add Swagger JSDoc for movies

--- src/movies/routes/index.js
+++ src/movies/routes/index.js
@@ -11,26 +11,160 @@ const createMoviesRouter = (dependencies) => {
      router.route('/*')
          .all(accountsController.verify); //ADD THIS: require token for all routes
 
-     router.route('/:id')
-         .get(moviesController.getMovie);
- 
-     router.route('/')
-         .get(moviesController.find); 
- 
-     router.route('/upcoming')
-         .get(moviesController.getUpcomingMovies);     
-     // Add new routes
-     router.route('/:id/images')
-         .get(moviesController.getMovieImages);
-     
-     router.route('/:id/cast')
-         .get(moviesController.getMovieCast);
-     
-     router.route('/:id/reviews')
-         .get(moviesController.getMovieReviews);
-     
-     router.route('/:id/similar')
-         .get(moviesController.getSimilarMovies);
+    /**
+     * @openapi
+     * /api/movies/{id}:
+     *   get:
+     *     tags:
+     *       - Movies
+     *     summary: Get movie by id
+     *     description: This endpoint returns movie details by movie ID
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         required: true
+     *         description: Movie ID
+     *         schema:
+     *           type: string
+     *     responses:
+     *       '200':
+     *         description: Successfully retrieved movie
+     *       '500':
+     *         description: Internal server error
+     */
+    router.route('/:id')
+        .get(moviesController.getMovie);
+
+    /**
+     * @openapi
+     * /api/movies/:
+     *   get:
+     *     tags:
+     *       - Movies
+     *     summary: Get movies
+     *     description: This endpoint returns all movies
+     *     responses:
+     *       '200':
+     *         description: Successfully retrieved movies
+     *       '500':
+     *         description: Internal server error
+     */
+    router.route('/')
+        .get(moviesController.find);
+
+    /**
+     * @openapi
+     * /api/movies/upcoming:
+     *   get:
+     *     tags:
+     *       - Movies
+     *     summary: Get upcoming movies
+     *     description: This endpoint returns upcoming movies
+     *     responses:
+     *       '200':
+     *         description: Successfully retrieved upcoming movies
+     *       '500':
+     *         description: Internal server error
+     */
+    router.route('/upcoming')
+        .get(moviesController.getUpcomingMovies);
+
+    /**
+     * @openapi
+     * /api/movies/{id}/images:
+     *   get:
+     *     tags:
+     *       - Movies
+     *     summary: Get movie's images
+     *     description: This endpoint returns images of the specified movie
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         required: true
+     *         description: Movie ID
+     *         schema:
+     *           type: string
+     *     responses:
+     *       '200':
+     *         description: Successfully retrieved movie's images
+     *       '500':
+     *         description: Internal server error
+     */
+    router.route('/:id/images')
+        .get(moviesController.getMovieImages);
+
+    /**
+     * @openapi
+     * /api/movies/{id}/cast:
+     *   get:
+     *     tags:
+     *       - Movies
+     *     summary: Get movie's cast
+     *     description: This endpoint returns cast of the specified movie
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         required: true
+     *         description: Movie ID
+     *         schema:
+     *           type: string
+     *     responses:
+     *       '200':
+     *         description: Successfully retrieved movie's cast
+     *       '500':
+     *         description: Internal server error
+     */
+    router.route('/:id/cast')
+        .get(moviesController.getMovieCast);
+
+    /**
+     * @openapi
+     * /api/movies/{id}/reviews:
+     *   get:
+     *     tags:
+     *       - Movies
+     *     summary: Get movie's reviews
+     *     description: This endpoint returns reviews of the specified movie
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         required: true
+     *         description: Movie ID
+     *         schema:
+     *           type: string
+     *     responses:
+     *       '200':
+     *         description: Successfully retrieved movie's reviews
+     *       '500':
+     *         description: Internal server error
+     */
+    router.route('/:id/reviews')
+        .get(moviesController.getMovieReviews);
+
+    /**
+     * @openapi
+     * /api/movies/{id}/similar:
+     *   get:
+     *     tags:
+     *       - Movies
+     *     summary: Get similar movies
+     *     description: This endpoint returns movies similar to the specified movie
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         required: true
+     *         description: Movie ID
+     *         schema:
+     *           type: string
+     *     responses:
+     *       '200':
+     *         description: Successfully retrieved similar movies
+     *       '500':
+     *         description: Internal server error
+     */
+    router.route('/:id/similar')
+        .get(moviesController.getSimilarMovies);
+
 
     return router;
 };
